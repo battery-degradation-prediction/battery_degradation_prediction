bc95886d44a53da4bac11f4bb57f5363078a8e4f
pohaoc2 <pohaoc2@uw.edu>
2023-02-27 23:09:40 -0800

    formatting and documenting preprocessing.py

--- battery_degradation_prediction/src/battery_degradation_prediction/preprocessing.py
+++ battery_degradation_prediction/src/battery_degradation_prediction/preprocessing.py
@@ -1,12 +1,13 @@
-import numpy as np
-import pandas as pd
+"""preprocessing data module"""
+from typing import Tuple
 from datetime import datetime
+import pandas as pd
 from load_data import load_data
 
 # func to convert datetimes in dataframe to time objects    WORKS
-def convert_datetime_str_to_obj(date_time_str:str):
-    """This function converts a string into a datatime object
-    
+def convert_datetime_str_to_obj(date_time_str: str) -> datetime:
+    """This function converts a string into a datatime object.
+
     Parameters
     ----------
     date_time_str : str
@@ -19,27 +20,56 @@ def convert_datetime_str_to_obj(date_time_str:str):
         A datetime object containing year, month, day, hour, minute, and second.
     """
 
-    time_object = datetime.strptime(date_time_str, '%Y-%m-%d-%H-%M-%S')
+    time_object = datetime.strptime(date_time_str, "%Y-%m-%d-%H-%M-%S")
     return time_object
 
+
 # func to remove current outliers (ie current that's is close to zero) from dataframe    NOT SURE
-def find_outlier(dataframe, column):
-    """
+def find_outlier(dataframe: pd.DataFrame, column: str) -> Tuple(pd.Series, pd.Series):
+    """Find the samples that are outside the lower and upper bound of IQR.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The dataframe containing outliers
+    column : str
+        The column name for searching outliers
+
+    Returns
+    -------
+    q_low : pd.Series
+        The pd.Series that are lower than 10% of the column.
+    q_high : pd.Series
+        The pd.Series that are higher than 90% of the column.
     """
     # Find first and third quartile
-    q1 = dataframe[column].quantile(0.1)  # return Series
-    q3 = dataframe[column].quantile(0.9)
+    quantile_1 = dataframe[column].quantile(0.1)  # return Series
+    quantile_3 = dataframe[column].quantile(0.9)
 
     # Find interquartile range
-    IQR = q3 - q1
+    iqr = quantile_3 - quantile_1
 
     # Find lower and upper bound
-    q_low = q1 - 1.5*IQR
-    q_high = q3 + 1.5*IQR
+    q_low = quantile_1 - 1.5 * iqr
+    q_high = quantile_3 + 1.5 * iqr
     return q_low, q_high
 
-def remove_outlier(dataframe, column):
-    """
+
+def remove_outlier(dataframe: pd.DataFrame, column: str) -> pd.DataFrame:
+    """Remove outliers in column of the dataframe and replace them with NaN, and the
+    inplace is always True.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The dataframe containing outliers
+    column : str
+        The column name for searching outliers
+
+    Returns
+    -------
+    dataframe : pd.DataFrame
+        The dataframe with a clean column.
     """
     # Find outliers Series
     q_low, q_high = find_outlier(dataframe, column)
@@ -50,31 +80,77 @@ def remove_outlier(dataframe, column):
 
     return dataframe
 
+
 # func to remove NaN in columns
-def remove_nan_from_data(dataframe):
+def remove_nan_from_data(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """Remove NaN from dataframe.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The dataframe containing outliers
+
+    Returns
+    -------
+    dataframe : pd.DataFrame
+        The dataframe with no NaNs in columns
+    """
     dataframe.dropna(subset=list(dataframe.columns.values))
     return dataframe
 
+
 # func to calc test time in hours
-def calc_test_time_from_datetime(target_time:datetime, start_time:datetime) -> float:
-    """
-    
+def calc_test_time_from_datetime(target_time: datetime, start_time: datetime) -> float:
+    """Compute elapsed time.
+
+    Parameters
+    ----------
+    target_time : datetime
+        target time
+    start_time : datetime
+        start time
+
     Returns
     -------
-    test_time : datetime.datetime
+    test_time : datetime
         unit: hour
     """
-    second_to_hour = 1./3600.
+
+    second_to_hour = 1.0 / 3600.0
     time_elapsed = (target_time - start_time).seconds * second_to_hour
     return time_elapsed
 
+
 # func to look only at the discharge cycles within dataframe
-def isolate_discharge_cyc_data(dataframe):
-    dataframe = dataframe[dataframe['type'] == 'discharging']
+def isolate_discharge_cyc_data(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """Return the dataframe with only discharge cycle.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The dataframe containing entire cycle data
+
+    Returns
+    -------
+    dataframe : pd.DataFrame
+        The dataframe containing only the discharging data
+    """
+
+    dataframe = dataframe[dataframe["type"] == "discharging"]
     return dataframe
 
-def add_elapsed_time_per_cycle(df:pd.DataFrame) -> list[float]:
-    """
+
+def add_elapsed_time_per_cycle(df: pd.DataFrame) -> list[float]:
+    """Return the elapsed time per cycle.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        The dataframe containing the continuous time column
+
+    Returns
+    -------
+    time_elasped_list : list[float]
     """
     time_elasped_list = []
     for time in df.groupby("cycle")["time"]:
@@ -84,24 +160,27 @@ def add_elapsed_time_per_cycle(df:pd.DataFrame) -> list[float]:
             time_elasped_list.append(time_elasped)
     return time_elasped_list
 
+
 def main():
-    path = '../data/B0005.csv'
+    """Main function"""
+    path = "../data/B0005.csv"
     df = load_data(path)
-    #df = df.iloc[13000:15000]
-    
+    # df = df.iloc[13000:15000]
+
     df["time"] = df["datetime"].apply(convert_datetime_str_to_obj)
-    df["elapsed_time"] = df["time"].apply(calc_test_time_from_datetime, args=(df['time'].iloc[0],))
+    df["elapsed_time"] = df["time"].apply(
+        calc_test_time_from_datetime, args=(df["time"].iloc[0],)
+    )
     df_discharge = isolate_discharge_cyc_data(df)
     df_discharge["elapsed_time_per_cycle"] = add_elapsed_time_per_cycle(df_discharge)
-    #remove_outlier(df, "current_measured")
-    
-    #add_time_elapsed_col(df_discharge)
+    # remove_outlier(df, "current_measured")
+
+    # add_time_elapsed_col(df_discharge)
     for group in df_discharge.groupby("cycle"):
         print(group[1].head())
         print("=" * 15)
-    return 
+    return df
+
 
 if __name__ == "__main__":
     main()
-
-
